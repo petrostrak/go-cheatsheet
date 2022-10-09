9df4a811ddb45c1130797430bb91b666a55fdb88
petrostrak <pit.trak@gmail.com>
2022-10-09 16:42:22 +0300

    Add DeletePerson grpc handler

--- Persistence/MongoDB/server/server.go
+++ Persistence/MongoDB/server/server.go
@@ -130,3 +130,32 @@ func (s *Server) UpdatePerson(ctx context.Context, in *pb.Person) (*emptypb.Empt
 
 	return &emptypb.Empty{}, nil
 }
+
+func (s *Server) DeletePerson(ctx context.Context, in *pb.PersonId) (*emptypb.Empty, error) {
+	log.Printf("DeletePerson() invoked with %v\n", in)
+
+	oid, err := primitive.ObjectIDFromHex(in.Id)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("error parse ID: %v\n", err),
+		)
+	}
+
+	res, err := collection.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Could not delete object in mongoDB: %v\n", err),
+		)
+	}
+
+	if res.DeletedCount == 0 {
+		return nil, status.Errorf(
+			codes.NotFound,
+			"Petros was not found",
+		)
+	}
+
+	return &emptypb.Empty{}, nil
+}
